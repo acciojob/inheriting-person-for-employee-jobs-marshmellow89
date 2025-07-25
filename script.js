9f0fa88fcb3561b538f8fc17eb6766dbedd7f5d9
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-25 14:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
+// // complete this js code
+// function Person(name, age) {
+// 	this.name = name; 
+// 	this.age = age;
 
-	this.greet = function(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	};
-}
+// 	this.greet = function(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+// 	};
+// }
 
-function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
+// function Employee(name, age, jobTitle) {
+// 	Person.call(this, name, age);
+// 	this.jobTitle = jobTitle;
 
-	this.jobGreet = function(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-	};
-}
+// 	this.jobGreet = function(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// 	};
+// }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+// Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype.constructor = Employee;
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
 
 

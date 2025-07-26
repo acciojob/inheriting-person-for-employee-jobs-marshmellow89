cd7db91bb90be094479b379403aa906166a961a0
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-26 15:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,3 +25,25 @@
 // window.Employee = Employee;
 
 
+function Person(name, age) {
+		this.name=name;
+		this.age=age;
+}
+Person.prototype.greet=function(){
+		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+	}
+ 
+function Employee (name, age, jobTitle) {
+		Person.call(this,name,age);
+		this.jobTitle=jobTitle;
+	}
+Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.constructor=Employee;
+	Employee.prototype.jobGreet=function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)		
+	}
+ 
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
+
